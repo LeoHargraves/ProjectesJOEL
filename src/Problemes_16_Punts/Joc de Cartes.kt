c12ef727e0d8e1025a95d2d6a07a661b387b6eb8
Leo <leo.castan.7e8@itb.cat>
2025-10-15 09:11:01 +0200

    any

--- src/Problemes_16_Punts/Joc de Cartes.kt
+++ src/Problemes_16_Punts/Joc de Cartes.kt
@@ -6,20 +6,74 @@ fun main() {
     val sc = Scanner(System.`in`)
     repeat(sc.nextInt()){
         val numCartes=sc.nextInt()
-        val jo= IntArray(numCartes){sc.nextInt()}.toMutableList()
-        val taula= IntArray(numCartes){sc.nextInt()}.toMutableList()
-        var contadorJo=0
-        while (contadorJo<jo.size){
-            var contadorTaula=0
-            while (contadorTaula<taula.size){
-                if (jo[contadorJo]>taula[contadorTaula]&&jo[contadorJo]<=taula[contadorTaula]){
-
-                }
-                contadorTaula++
-            }
-            contadorJo++
+        val joUn= IntArray(numCartes){sc.nextInt()}.toMutableList()
+        val taulaUn= IntArray(numCartes){sc.nextInt()}.toMutableList()
+        val jo=joUn.sorted().toMutableList()
+        val taula=taulaUn.sorted().toMutableList()
+        var puntuacio=0
+        puntuacio+=calculateWins(jo,taula)
+        calculateDraws(jo,taula)
+        puntuacio-=taula.size
+        if (puntuacio>0){
+            println("Victoria")
+        }else if (puntuacio==0){
+            println("Empate")
+        }else{
+            println("Derrota")
         }
     }
 
     sc.close()
+}
+
+fun calculateWins(jo: MutableList<Int>, taula: MutableList<Int>):Int{
+    var puntuacio=0
+    var contadorJo=0
+    while (contadorJo<jo.size){
+        var contadorTaula=0
+        while (contadorTaula<taula.size){
+            if (contadorTaula==taula.size-1&&jo[contadorJo]>taula[contadorTaula]){
+                taula.removeAt(contadorTaula)
+                jo.removeAt(contadorJo)
+                puntuacio++
+                contadorTaula=taula.size
+                contadorJo--
+            }else if (jo[contadorJo]>taula[contadorTaula]&&jo[contadorJo]<=taula[contadorTaula+1]){
+                taula.removeAt(contadorTaula)
+                jo.removeAt(contadorJo)
+                puntuacio++
+                contadorTaula=taula.size
+                contadorJo--
+            }else if(jo[contadorJo]<=taula[contadorTaula]){
+                contadorTaula=taula.size
+            }
+            contadorTaula++
+        }
+        contadorJo++
+    }
+    return puntuacio
+}
+
+fun calculateDraws(jo: MutableList<Int>, taula: MutableList<Int>){
+    var contadorJo=0
+    while (contadorJo<jo.size){
+        var contadorTaula=0
+        while (contadorTaula<taula.size){
+            if (contadorTaula==taula.size-1&&jo[contadorJo]==taula[contadorTaula]){
+                taula.removeAt(contadorTaula)
+                jo.removeAt(contadorJo)
+                contadorTaula=taula.size
+                contadorJo--
+            }else if (jo[contadorJo]==taula[contadorTaula]&&jo[contadorJo]<taula[contadorTaula+1]){
+                taula.removeAt(contadorTaula)
+                jo.removeAt(contadorJo)
+                contadorTaula=taula.size
+                contadorJo--
+            }else if(jo[contadorJo]<taula[contadorTaula]){
+                contadorTaula=taula.size
+            }
+            contadorTaula++
+        }
+        contadorJo++
+    }
 }
