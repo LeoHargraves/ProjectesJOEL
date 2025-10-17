45150c2649e9b107cce54918c41542583f85aea9
Leo <leo.castan.7e8@itb.cat>
2025-10-17 11:26:08 +0200

    any

---
+++ src/Problemes_15_Punts/Capturant ocells.kt
@@ -0,0 +1,24 @@
+package Problemes_15_Punts
+
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val numOcells=sc.nextInt()
+    val ocells= IntArray(numOcells){sc.nextInt()}.toMutableList()
+    var numGabies=0
+    while (ocells.isNotEmpty()){
+        var alssadaGabia=ocells.first()
+        var ocellsIndex=0
+        while (ocellsIndex<ocells.size){
+            if (alssadaGabia==ocells.get(ocellsIndex)){
+                ocells.removeAt(ocellsIndex)
+                alssadaGabia--
+            }else{
+                ocellsIndex++
+            }
+        }
+        numGabies++
+    }
+    println(numGabies)
+}
