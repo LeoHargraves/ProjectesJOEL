269d8b68c61ca85c6614d238137ded8ebd7639a0
Leo <leo.castan.7e8@itb.cat>
2025-10-20 13:03:32 +0200

    Wrong:Taula Cremada Simple,Penjat2 Good but error:Calentamiento Global, Ensamblador

---
+++ src/Problemes_15_Punts/De vacances a Gavà.kt
@@ -0,0 +1,16 @@
+package Problemes_15_Punts
+
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    var total=listOf(0, 0, 0)
+    repeat(sc.nextInt()){
+        val piscina=sc.nextInt()
+        val platja=sc.nextInt()
+        val futbol=sc.nextInt()
+
+    }
+    println()
+    sc.close()
+}
