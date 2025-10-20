75305c320fb910c45fac9425a259b644884416d9
leo.castan.7e8 <leo.castan.7e8@itb.cat>
2025-10-20 13:59:56 +0200

    sgfh

--- src/Problemes_15_Punts/De vacances a Gavà.kt
+++ src/Problemes_15_Punts/De vacances a Gavà.kt
@@ -4,12 +4,20 @@ import java.util.Scanner
 
 fun main() {
     val sc = Scanner(System.`in`)
-    var total=listOf(0, 0, 0)
+    var total= mutableListOf(0, 0, 0)
     repeat(sc.nextInt()){
         val piscina=sc.nextInt()
         val platja=sc.nextInt()
         val futbol=sc.nextInt()
-
+        if (it==0){
+            total[0]=piscina
+            total[1]=platja
+            total[2]=futbol
+        }else{
+            total[0]=piscina
+            total[1]=platja
+            total[2]=futbol
+        }
     }
     println()
     sc.close()
