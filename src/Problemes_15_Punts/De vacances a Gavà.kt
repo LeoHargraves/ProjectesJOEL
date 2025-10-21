23b65dce4434c58d75c97889d9006f6849c28de8
Leo <leo.castan.7e8@itb.cat>
2025-10-21 10:45:34 +0200

    any

--- src/Problemes_15_Punts/De vacances a Gavà.kt
+++ src/Problemes_15_Punts/De vacances a Gavà.kt
@@ -1,24 +1,38 @@
 package Problemes_15_Punts
 
 import java.util.Scanner
+import kotlin.math.max
 
 fun main() {
     val sc = Scanner(System.`in`)
-    var total= mutableListOf(0, 0, 0)
-    repeat(sc.nextInt()){
+    val casos=sc.nextInt()
+    var total= mutableListOf(sc.nextInt(), sc.nextInt(), sc.nextInt())
+    repeat(casos-1){
         val piscina=sc.nextInt()
         val platja=sc.nextInt()
         val futbol=sc.nextInt()
-        if (it==0){
-            total[0]=piscina
-            total[1]=platja
-            total[2]=futbol
+        if(platja<piscina){
+            total[0]=total[2]+piscina
         }else{
-            total[0]=piscina
-            total[1]=platja
-            total[2]=futbol
+            total[1]=total[2]+platja
+        }
+        if (piscina>futbol){
+            if (total[1]>total[2]){
+                total[0]=total[1]+piscina
+            }
+        }else{
+            total[2]=total[1]+futbol
+        }
+        if (futbol<platja){
+            if (total[0]>total[2]){
+                total[1]=total[0]+platja
+            }
+        }else{
+            if (total[0]>total[1]){
+                total[2]=total[0]+futbol
+            }
         }
     }
-    println()
+    println(total.joinToString(", "))
     sc.close()
 }
