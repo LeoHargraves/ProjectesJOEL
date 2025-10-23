9af2bc5807d21bda80f37f750350a888a3da2643
Leo <leo.castan.7e8@itb.cat>
2025-10-23 12:01:50 +0200

    Wrong:Taula Cremada Simple,Penjat2 Good but error:Calentamiento Global, Ensamblador

--- src/Problemes_30_Punts/Zoo.kt
+++ src/Problemes_30_Punts/Zoo.kt
@@ -4,28 +4,37 @@ import java.util.Scanner
 
 fun main() {
     val sc= Scanner(System.`in`)
-    val maxDepth=replaceIsland('T',0,0,1)
-
-
+    var matriu=emptyArray<CharArray>()
+    val rows=sc.nextInt()
+    val cols=sc.nextInt()
+    sc.nextLine()
+    repeat(rows) {
+        val line = sc.nextLine()
+        matriu += line.toCharArray()
+    }
+    val maxDepth=replaceIsland(matriu,0,0,0)
+    println(maxDepth)
     sc.close()
 }
 
-fun replaceIsland(grid: Array<CharArray>, row: Int, col: Int,maxDepth:Int) {
+fun replaceIsland(grid: Array<CharArray>, row: Int, col: Int,maxDepth:Int):Int {
     val newChar='X'
+    var newMaxDepth=maxDepth+1
     val rows = grid.size
     val cols = grid[0].size
     val originalChar = grid[row][col]
-
-    if (originalChar == newChar) return
-
+    if (originalChar == newChar) return 0
     fun dfs(r: Int, c: Int) {
         if (r !in 0 until rows || c !in 0 until cols) return
         if (grid[r][c] != originalChar) {
             if (grid[r][c]=='T'||grid[r][c]=='B'){
-                replaceIsland(grid,r,c)
+                val possible=replaceIsland(grid,r,c,newMaxDepth)
+                if (possible > newMaxDepth){
+                    newMaxDepth=possible
+                }
+            }else{
+                return
             }
-        }else{
-            return
         }
 
         grid[r][c] = newChar
@@ -37,4 +46,13 @@ fun replaceIsland(grid: Array<CharArray>, row: Int, col: Int,maxDepth:Int) {
     }
 
     dfs(row, col)
+    comprovarMatriu(grid)
+    println()
+    return newMaxDepth
+}
+
+fun comprovarMatriu(matriu: Array<CharArray>){
+    for (x in matriu){
+        println(x.joinToString(" "))
+    }
 }
