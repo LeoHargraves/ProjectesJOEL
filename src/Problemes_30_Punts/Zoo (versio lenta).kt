4741960f077bcb4a9da403d0ec9b448d3accd461
Leo <leo.castan.7e8@itb.cat>
2025-10-24 10:45:29 +0200

    Wrong:Taula Cremada Simple,Penjat2 Good but error:Calentamiento Global, Ensamblador

---
+++ src/Problemes_30_Punts/Zoo (versio lenta).kt
@@ -0,0 +1,67 @@
+package Problemes_30_Punts
+
+import java.util.Scanner
+
+fun main() {
+    val sc= Scanner(System.`in`)
+    var matriu=emptyArray<CharArray>()
+    val rows=sc.nextInt()
+    val cols=sc.nextInt()
+    sc.nextLine()
+    repeat(rows) {
+        val line = sc.nextLine()
+        matriu += line.toCharArray()
+    }
+    printColoredGrid(matriu)
+    println(countDepth(matriu))
+    sc.close()
+}
+
+fun countDepth(matriu:Array<CharArray>):Int{
+    var paintNext=matriu[0][0]
+    var profundidad=0
+    while (paintNext=='T'||paintNext=='B'){
+        paintNext=replaceBoard(matriu, if (profundidad % 2 == 0) '0' else '1',paintNext)
+        profundidad++
+    }
+    return profundidad
+}
+
+fun replaceBoard(matriu: Array<CharArray>, newChar:Char, animalActual: Char): Char {
+    val rows = matriu.size
+    val cols = matriu[0].size
+    var paintNext='X'
+    var altreAnimal='T'
+    var oldChar='0'
+    if (newChar=='0'){
+        oldChar='1'
+    }
+    if (animalActual=='T'){
+        altreAnimal='B'
+    }
+
+    //if (originalChar == newChar) return  // no need to replace if same
+
+    fun dfs(r: Int, c: Int) {
+        if (r !in 0 until rows || c !in 0 until cols) return
+        if (matriu[r][c] == altreAnimal) {
+            paintNext=altreAnimal
+            return
+        }
+        if (matriu[r][c] == animalActual||matriu[r][c] == oldChar){
+            matriu[r][c] = newChar
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        }else{
+            return
+        }
+    }
+
+    dfs(0, 0)
+    printColoredGrid(matriu)
+    println()
+    return paintNext
+}
--- src/Problemes_30_Punts/Zoo.kt
+++ src/Problemes_30_Punts/Zoo.kt
@@ -22,16 +22,16 @@ fun replaceIsland(matriu: Array<CharArray>, row: Int, col: Int, maxDepth:Int):In
     var newMaxDepth=maxDepth+1
     val rows = matriu.size
     val cols = matriu[0].size
-    var seguentRows=emptyList<Int>()
-    var seguentCols=emptyList<Int>()
+    val seguentRows=mutableListOf<Int>()
+    val seguentCols=mutableListOf<Int>()
     val originalChar = matriu[row][col]
     if (originalChar == newChar) return 0
     fun dfs(r: Int, c: Int) {
         if (r !in 0 until rows || c !in 0 until cols) return
         if (matriu[r][c] != originalChar) {
             if (matriu[r][c]=='T'||matriu[r][c]=='B'){
-                seguentRows=seguentRows.plus(r)
-                seguentCols=seguentCols.plus(c)
+                seguentRows.add(r)
+                seguentCols.add(c)
                 return
             }else{
                 return
@@ -45,19 +45,44 @@ fun replaceIsland(matriu: Array<CharArray>, row: Int, col: Int, maxDepth:Int):In
         dfs(r, c + 1)
         dfs(r, c - 1)
     }
+    printColoredGrid(matriu)
+
 
     dfs(row, col)
+    val opcionsProf=mutableListOf<Int>()
     for (i in 0 until seguentCols.size){
-        val possible=replaceIsland(matriu,seguentRows[i],seguentCols[i],newMaxDepth)
-        if (possible > newMaxDepth){
-            newMaxDepth=possible
-        }
+        opcionsProf.add(replaceIsland(matriu,seguentRows[i],seguentCols[i], maxDepth))
     }
+    if (opcionsProf.isNotEmpty()){
+        newMaxDepth+=opcionsProf.max()
+    }
+    println("\n$newMaxDepth\n")
     return newMaxDepth
 }
 
 fun comprovarMatriu(matriu: Array<CharArray>){
     for (x in matriu){
         println(x.joinToString(" "))
+    }
+}
+
+fun printColoredGrid(grid: Array<CharArray>) {
+    val reset = "\u001B[0m"
+
+    // Helper function for background color (24-bit RGB)
+    fun bg(r: Int, g: Int, b: Int) = "\u001B[48;2;${r};${g};${b}m"
+
+    for (row in grid) {
+        for (cell in row) {
+            val color = when (cell) {
+                '*' -> bg(120, 120, 120)  // gray
+                'T' -> bg(0, 180, 0)      // green
+                'B' -> bg(200, 180, 0)    // yellow
+                'X' -> bg(200, 0, 0)      // red
+                else -> bg(0, 0, 0)       // black for unknown
+            }
+            print("$color  $reset")  // two spaces for square shape
+        }
+        println()
     }
 }
