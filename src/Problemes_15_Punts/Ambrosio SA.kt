50fdf5ebba429c268b52b305a802391012374b03
LeoHargraves <leo.castan.7e8@itb.cat>
2025-11-06 21:53:39 +0100

    Wrong:Taula Cremada Simple(11),Penjat2(8) Good but error:Calentamiento Global(11), Ensamblador(12) In process:David compra(20), Taula Cremada(50)

--- src/Problemes_15_Punts/Ambrosio SA.kt
+++ src/Problemes_15_Punts/Ambrosio SA.kt
@@ -7,20 +7,60 @@ fun main() {
     val casos=sc.nextInt()
     repeat(casos){
         val tamany=sc.nextInt()
-        val maxValorsPerPis= IntArray(tamany){0}
-        for (i in 1..tamany){
-            if (i==1||i==tamany){
+        val matriu=Array(tamany){Array(tamany){sc.nextInt()}}
+        val center=tamany/2
+        var dades=Dades(matriu[center][center],matriu[center][center],true)
+        for (layer in 1..center) {
+            val start = center - layer
+            val end = center + layer
 
+            for (c in start..end) {
+                dades=comprovar(matriu,dades,start,c)
             }
+            for (r in start + 1..end) {
+                dades=comprovar(matriu,dades,r, end)
+            }
+            for (c in end - 1 downTo start) {
+                dades=comprovar(matriu,dades,end, c)
+            }
+            for (r in end - 1 downTo start + 1) {
+                dades=comprovar(matriu,dades,r, start)
+            }
+            dades=dades.update()
+        }
+        if (dades.out){
+            println("OK")
+        }else{
+            println("NO")
         }
     }
     sc.close()
 }
 
-fun convertir(num:Int,tamany:Int):Int{
-    var output=num-tamany
-    if(output<0){
-        output*-1
+fun comprovar(matriu:Array<Array<Int>>,dades:Dades, x:Int, y:Int):Dades{
+    var output=dades
+    if (matriu[x][y]<=dades.ant){
+        output= dades.over()
+    }
+    if (matriu[x][y]>output.act){
+        output=output.max(matriu[x][y])
     }
     return output
+}
+
+class Dades(anterior:Int,actual:Int,output:Boolean){
+    val ant=anterior
+    val act=actual
+    val out=output
+    fun update():Dades{
+        return Dades(act,act,out)
+    }
+
+    fun max(new:Int):Dades{
+        return Dades(ant,new,out)
+    }
+
+    fun over():Dades{
+        return Dades(ant,act,false)
+    }
 }
