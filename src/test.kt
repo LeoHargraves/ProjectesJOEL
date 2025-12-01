d553ef80914ac4249c470dc61be819e305bb6e45
Leo <leo.castan.7e8@itb.cat>
2025-12-01 11:29:28 +0100

    Wrong:Taula Cremada Simple,Penjat2 Good but error:Calentamiento Global, Ensamblador

--- src/test.kt
+++ src/test.kt
@@ -3,21 +3,5 @@ import java.util.*
 
 fun main() {
     val sc = Scanner(System.`in`)
-    val n=sc.nextInt()
-    val matriu= Array(n){ CharArray(n) }
-    for (y in matriu.indices){
-        for (x in matriu[y].indices){
-            val int=sc.nextInt()
-            if (int==13){
-                matriu[y][x]='x'
-            }else{
-                matriu[y][x]='o'
-            }
-        }
-    }
-    println()
-    for (line in matriu){
-        println(line.joinToString(" | "))
-        println("____________________________________________")
-    }
+    println(Int.MAX_VALUE)
 }
